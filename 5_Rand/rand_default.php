a77ccd5a62007d4b81e5cff0dbf306d8e64f43a0
Choe Hyeong Jin <chj6097@gmail.com>
2017-11-24 11:59:53 +0900

    Update rand_default

--- 5_Rand/rand_default.php
+++ 5_Rand/rand_default.php
@@ -3,6 +3,6 @@
   // Written by Choe Hyeong Jin, Dept. of CSE, Univ. of Seoul
   // php 4.2.0부터 난수값 생성기 초기화를 할 필요가 없다.
   for($i=1;$i<=500;$i++)
-    echo $i.' '.rand(1, 500).'
+    echo $i.':'.rand(1, 500).'
 ';
 ?>
