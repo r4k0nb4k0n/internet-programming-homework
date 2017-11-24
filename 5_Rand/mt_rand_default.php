2364ab0b1d612240b823df00852355aedb7c6585
Choe Hyeong Jin <chj6097@gmail.com>
2017-11-24 12:31:20 +0900

    Update mt_rand_default

--- 5_Rand/mt_rand_default.php
+++ 5_Rand/mt_rand_default.php
@@ -2,7 +2,7 @@
   // 2017-2 Internet-Programming
   // Written by Choe Hyeong Jin, Dept. of CSE, Univ. of Seoul
   // Given a seed value using microtime()
-  for($i=0;$i<500;$i++)
-    echo mt_rand(1, 500).'
+  for($i=1;$i<=500;$i++)
+    echo $i.';'.mt_rand(1, 500).'
 ';
 ?>
