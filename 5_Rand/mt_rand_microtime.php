f3bb5fa148b3d8e96462b0048d523b6d04912bbd
Choe Hyeong Jin <chj6097@gmail.com>
2017-11-24 12:38:42 +0900

    Update mt_rand_microtime

--- 5_Rand/mt_rand_microtime.php
+++ 5_Rand/mt_rand_microtime.php
@@ -2,8 +2,8 @@
   // 2017-2 Internet-Programming
   // Written by Choe Hyeong Jin, Dept. of CSE, Univ. of Seoul
   // Given a seed value using microtime()
-  mt_srand((double)microtime()*10000);
-  for($i=0;$i<500;$i++)
-    echo mt_rand(1, 500).'
+  mt_srand((double)microtime()*1000);
+  for($i=1;$i<=500;$i++)
+    echo $i.';'.mt_rand(1, 500).'
 ';
 ?>
